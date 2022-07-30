6b6a619b655ca495c09c595d8c8ca7a5fe26c433
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-30 19:39:35 +0430

    sections:feat(section):create section page

---
+++ pages/section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Sections from '../src/components/Sections'
+
+
+function Section() {
+  return (
+    <div className='h-full'>
+        <Sections/>
+    </div>
+  )
+}
+
+export default Section
