dd536f8c020ef434e49b4342641f7418a3527984
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-30 23:18:06 +0430

    connections:feat(SectionTitle):add className property

--- src/components/SectionTitle/index.jsx
+++ src/components/SectionTitle/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function SectionTitle({ title }) {
+function SectionTitle({ title , className=""}) {
   return (
-    <span className="absolute left-[12px] px-3 top-[-13px] section-title ">
+    <span className={`absolute left-[12px] px-3 top-[-13px] section-title ${className}`}>
       {title}
     </span>
   );
