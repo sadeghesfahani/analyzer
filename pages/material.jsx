f055eae23250145a5cfb4afe42dd96fb6018ac20
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-07-30 05:09:56 +0430

    feat(material):create material page

---
+++ pages/material.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import MaterialProperties from '../src/components/MaterialProperties'
+
+function Material() {
+  return (
+    <div>
+        <MaterialProperties/>
+    </div>
+  )
+}
+
+export default Material
