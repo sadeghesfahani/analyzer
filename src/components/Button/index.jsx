17b5325d17a29ea7c945d406289284bf77a5dbf2
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-01 01:01:55 +0430

    feat(Button):add onClick property

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Button({title, className}) {
+function Button({title, className, onClick}) {
   return (
-    <button className={`bg-[#f2f2f2] p-1 px-3 relative ${className}`}>
+    <button className={`bg-[#f2f2f2] p-1 px-3 relative ${className}`} onClick={onClick}>
       <div className="bg-blue-300 w-full h-full opacity-25 blur absolute"></div>
       <span>{title}</span>
     </button>
