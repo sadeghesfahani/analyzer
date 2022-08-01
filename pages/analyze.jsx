312d14479d663787de70254d24744814a5509cb8
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-01 05:05:41 +0430

    feat(analyze):use useSelectorand map on analyze

--- pages/analyze.jsx
+++ pages/analyze.jsx
@@ -3,16 +3,19 @@ import AnalyzeCheckBox from '../src/components/AnalyzeCheckBox'
 import Button from '../src/components/Button'
 import SectionTitle from '../src/components/SectionTitle'
 
+import { useSelector } from 'react-redux'
+
 function Analyze() {
+    const { analyze } = useSelector(state => state.analyze)
   return (
     <div className='h-full p-1'>
         <div className='border border-black gap-4 p-3 flex flex-col'>
             <div className='border border-black relative p-4'>
                 <SectionTitle title='Analyze' className='text-sm'/>
                 <div className='flex flex-col gap-4'>
-                    <AnalyzeCheckBox title="Design for Ultimate limit state"/>
-                    <AnalyzeCheckBox title="Design for serviceability"/>
-                    <AnalyzeCheckBox title="Seismic performance factors"/>
+                    {analyze.map((item) => (
+                        <AnalyzeCheckBox key={item.name} name={item.name} checked={item.check}/>
+                    ))}
                 </div>
             </div>
             <div className='border border-black py-4 flex justify-center items-center'>
