068c1cfc86d3978649168de0b7c524d15950df21
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-02 04:22:17 +0430

    edit(preload):change preload

--- public/preload.js
+++ public/preload.js
@@ -1,5 +1,5 @@
-const { contextBridge } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electron',{
-    ipcRenderer : require('electron').ipcRenderer,
+    ipcRenderer,
 })
