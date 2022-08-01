a1b942d31222d507ce5d7b7e2280876dc4458733
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-01 05:07:20 +0430

    feat(store):add slices

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import materialReducer from "./slices/material";
+import sectionsReducer from "./slices/sections";
+import connectionsSlice from "./slices/connections";
+import seismicResistingFramesTypeReducer from "./slices/seismicResistingFramesType";
+import structuresPropertyReducer from "./slices/structuresProperty";
+import analyzeReducer from "./slices/analyze";
+import propertiesSlice from "./slices/properties";
 
 
 export default configureStore({
     reducer:{
-        material: materialReducer
+        material: materialReducer,
+        sections: sectionsReducer,
+        connections: connectionsSlice,
+        seismicResistingFramesType: seismicResistingFramesTypeReducer,
+        structuresProperty: structuresPropertyReducer,
+        analyze: analyzeReducer,
+        properties:propertiesSlice
     }   
 })
