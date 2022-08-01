27cb1cce5b7dbc19068bb75138d992e6315973de
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-01 05:04:25 +0430

    feat:add actions

--- pages/structuresProperties.jsx
+++ pages/structuresProperties.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import Button from '../src/components/Button'
 import SectionTitle from '../src/components/SectionTitle'
 
+import { useSelector, useDispatch } from 'react-redux'
+import {setHeightOfStorey, setLengthOfSpan} from "../redux/slices/structuresProperty";
+
+
+
 function StructuresProperties() {
+    const dispatch = useDispatch()
+    const { heightOfStorey, lengthOfSpan } = useSelector(state => state.structuresProperty)
   return (
     <div className='p-4 flex gap-4 h-full'>
         <div className='flex flex-col gap-4 flex-[0.6]'>
@@ -10,11 +17,11 @@ function StructuresProperties() {
                 <SectionTitle title="Structure's property"/>
                 <div className='flex gap-4'>
                     <h6>Height of storey :</h6>
-                    <input type="text" />
+                    <input value={heightOfStorey} type="text" onChange={(e)=>{dispatch(setHeightOfStorey(e.target.value))}}/>
                 </div>
                 <div className='flex gap-4'>
                     <h6>Length of span :</h6>
-                    <input type="text" />
+                    <input type="text" value={lengthOfSpan} onChange={(e)=>dispatch(setLengthOfSpan(e.target.value))}/>
                 </div>
             </div>
             <div className='border border-black py-4 flex justify-center items-center'>
--- src/components/Connections/index.jsx
+++ src/components/Connections/index.jsx
@@ -1,41 +1,102 @@
-import React from 'react'
-import ConnectionsCheckBox from '../ConnectionsCheckBox'
-import ConnectionsTable from '../ConnectionsTable'
-import SectionTitle from '../SectionTitle'
-import Button from "../Button"
+import React from "react";
+import ConnectionsCheckBox from "../ConnectionsCheckBox";
+import ConnectionsTable from "../ConnectionsTable";
+import SectionTitle from "../SectionTitle";
+import Button from "../Button";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  editInterModularConnection,
+  editIntraModularConnection,
+} from "../../../redux/slices/connections";
 
 function Connections() {
+  const { interModularConnection, intraModularConnection } = useSelector(
+    (store) => store.connections
+  );
+  const [selectValue, setSelectValue] = React.useState(
+    "Inter modular connection"
+  );
   return (
-    <div className='m-1 border border-black p-4 flex flex-col gap-4 h-full'>
-        <div className='flex justify-between'>
-            <div className='border border-black relative w-[150px]'>
-                <SectionTitle title='Direction' className='text-sm'/>
-                <div className='px-2 py-3 flex flex-col gap-2'>
-                    <ConnectionsCheckBox title='U1'/>
-                    <ConnectionsCheckBox title='U2'/>
-                    <ConnectionsCheckBox title='U3'/>
-                </div>
-            </div>
-            <div className='border border-black relative w-[150px]'>
-                <SectionTitle title='Direction' className='text-sm'/>
-                <div className='px-2 py-3 flex flex-col gap-2'>
-                    <ConnectionsCheckBox title='R1'/>
-                    <ConnectionsCheckBox title='R2'/>
-                    <ConnectionsCheckBox title='R3'/>
-                </div>
-            </div>
+    <div className="m-1 border border-black p-4 flex flex-col gap-4 h-full">
+      <div>
+        <select
+          value={selectValue}
+          onChange={(e) => setSelectValue(e.target.value)}
+        >
+          <option value={"Inter modular connection"}>
+            Inter modular connection
+          </option>
+          <option value={"Intra modular connection"}>
+            Intra modular connection
+          </option>
+        </select>
+      </div>
+      <div className="flex justify-between">
+        <div className="border border-black relative w-[150px]">
+          <SectionTitle title="Direction" className="text-sm" />
+          <div className="px-2 py-3 flex flex-col gap-2">
+            {selectValue === "Inter modular connection"
+              ? interModularConnection
+                  .filter((c) => c.name[0] === "U")
+                  .map((item) => (
+                    <ConnectionsCheckBox
+                      key={item.name}
+                      name={item.name}
+                      check={item.check}
+                      value={item.value}
+                      onChange={editInterModularConnection}
+                    />
+                  ))
+              : intraModularConnection
+                  .filter((c) => c.name[0] === "U")
+                  .map((item) => (
+                    <ConnectionsCheckBox
+                      key={item.name}
+                      name={item.name}
+                      check={item.check}
+                      value={item.value}
+                      onChange={editIntraModularConnection}
+                    />
+                  ))}
+          </div>
         </div>
-        <div className='border border-black relative p-2'>
-            <SectionTitle title='Properties' className='text-sm'/>
-            <div className='bg-white border overflow-x-auto border-black'>
-                <ConnectionsTable/>
-            </div>
+        <div className="border border-black relative w-[150px]">
+          <SectionTitle title="Direction" className="text-sm" />
+          <div className="px-2 py-3 flex flex-col gap-2">
+          {selectValue === "Inter modular connection"
+              ? interModularConnection
+                  .filter((c) => c.name[0] === "R")
+                  .map((item) => (
+                    <ConnectionsCheckBox
+                      key={item.name}
+                      name={item.name}
+                      check={item.check}
+                      value={item.value}
+                      onChange={editInterModularConnection}
+                    />
+                  ))
+              : intraModularConnection
+                  .filter((c) => c.name[0] === "R")
+                  .map((item) => (
+                    <ConnectionsCheckBox
+                      key={item.name}
+                      name={item.name}
+                      check={item.check}
+                      value={item.value}
+                      onChange={editIntraModularConnection}
+                    />
+                  ))}
+          </div>
         </div>
-        <div className='border border-black relative p-2 px-6 self-center'>
-            <Button title='Save and Close' className='border border-black text-sm px-4'/>
-        </div>
+      </div>
+      <div className="border border-black relative p-2 px-6 self-center">
+        <Button
+          title="Save and Close"
+          className="border border-black text-sm px-4"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Connections
+export default Connections;
