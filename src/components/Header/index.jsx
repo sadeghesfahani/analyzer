9234d5df61e43db047ec9f2b4aa1e30758e21205
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-08-02 01:15:11 +0430

    article button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,19 +1,24 @@
 import React from "react";
-
-const btnStyle = "h-full text-white min-w-[5rem] px-4 font-semibold text-sm  bg-red-900 rounded-sm hover:bg-red-700 hover:transition-all duration-200 ease-in-out";
-
+import {MdOutlineArticle} from "react-icons/md";
+const btnStyle = "h-full text-white min-w-[5rem] px-4 font-semibold text-sm  rounded-sm hover:transition-all duration-800 ease-in-out ";
+const redBtn = " bg-[#3e3f42]  hover:bg-[#535456]";
 export default function Header() {
     return (
-        <header className="flex w-full h-14 z-1 justify-between items-center absolute top-0 left-0 right-0 bg-red-800 px-4">
-            <div className="flex h-full py-2 gap-2">
-                <button className={btnStyle}>
+        <header className="flex w-full h-14 z-1 justify-between items-center absolute top-0 left-0 right-0 bg-[#292A2D] px-4 py-2.5">
+            <div className="flex h-full  gap-2">
+                <button className={btnStyle + redBtn}>
                     test
                 </button>
-                <button className={btnStyle}>
+                <button className={btnStyle + redBtn}>
                     long ass transition
                 </button>
             </div >
-            <div className="flex h-full"></div>
+            <div className="flex h-full">
+                <button className={btnStyle + " bg-blue-600  hover:bg-blue-700 flex gap-1 items-center justify-center"}>
+                    articles
+                    <MdOutlineArticle className="relative top-0.5 text-[18px]"/>
+                </button>
+            </div>
         </header>
     )
 }
