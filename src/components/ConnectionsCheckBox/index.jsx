29a914e55d8344427faf80ab419a8635d43d44bc
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-02 17:47:25 +0430

    y-result:edit(ConnectionsCheckBox):delete checkbox

--- src/components/ConnectionsCheckBox/index.jsx
+++ src/components/ConnectionsCheckBox/index.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
-function ConnectionsCheckBox({name, check, value, onChange}) {
+function ConnectionsCheckBox({name, value, onChange}) {
   const dispatch = useDispatch();
   return (
-    <div className="flex items-center justify-between">
+    <div className="flex items-center gap-1 justify-between">
       <div className="flex items-center gap-1">
-        <input type="checkbox" className="p-4" id={`id-${name}`} checked={check} onChange={(e)=>dispatch(onChange({name,check:e.target.checked}))}/>
         <label htmlFor={`id-${name}`}>{name}</label>
+        <input type="text" className="border border-black w-full" id={`id-${name}`} value={value}  onChange={(e)=>dispatch(onChange({name,value:e.target.value}))} />
       </div>
-      <input type="text" className="border border-black w-1/2" value={value}  onChange={(e)=>dispatch(onChange({name,value:e.target.value}))} />
+      <span className="text-[12px]">{name[0] === "U" ? "N*mm/rad" : "N/mm"}</span>
     </div>
   );
 }
