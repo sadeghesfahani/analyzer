2d5b60114e4ba58b4dfedc2a40a63c377380a542
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-04 02:49:43 +0430

    y-result:feat:create page

---
+++ pages/stabilityResult.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Result from "../src/components/Result"
+
+function StabilityResult() {
+  return (
+    <div className='bg-white h-full'>
+      <Result/>
+    </div>
+  )
+}
+
+export default StabilityResult
