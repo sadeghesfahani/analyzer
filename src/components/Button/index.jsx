1142e0a300164ce4c9e838be60f69ce9138b3de2
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-08-04 16:21:29 +0430

    minor changes to button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Button({title, className, onClick}) {
   return (
-    <button className={`bg-blue-500 py-2 px-3 relative text-sm rounded text-white hover:bg-blue-700 ${className}`} onClick={onClick}>
+    <button className={`bg-blue-600 py-2 px-3 relative text-sm rounded font-semibold text-white hover:bg-blue-700 ${className}`} onClick={onClick}>
       {title}
     </button>
   );
