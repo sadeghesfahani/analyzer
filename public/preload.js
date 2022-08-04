54790648f3ad30b790dc68b7232016caa69cd870
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-05 04:17:51 +0430

    y-delete-menu:feat(preload):add openFile and closeFile

--- public/preload.js
+++ public/preload.js
@@ -2,4 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electron',{
     ipcRenderer,
+    openFile:(handler)=>ipcRenderer.on('open-file-handler',(event,arg)=>handler(arg)),
+    closeFile:(handler)=>ipcRenderer.on('close-file-handler',(event,arg)=>handler(arg)),
 })
