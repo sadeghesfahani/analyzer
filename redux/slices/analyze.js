09ca250dc7390ec5776b2dc38f2dc1406db6956f
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-15 00:47:25 +0430

    feat(slices/analyze):toggle action changed

--- redux/slices/analyze.js
+++ redux/slices/analyze.js
@@ -4,7 +4,7 @@ const analyzeSlice = createSlice({
     name: "analyze",
     initialState: {
         analyze: [
-            {name: 'design for Ultimate limit state', check:false},
+            {name: 'Design for ultimate limit state', check:false},
             {name: 'Design for serviceability', check:false},
             {name: 'Seismic performance factors', check:false},
         ]
@@ -15,7 +15,21 @@ const analyzeSlice = createSlice({
         },
         toggleAnalyze: (state, action) => {
             const index = state.analyze.findIndex((item) => item.name === action.payload.name);
-            state.analyze[index].check = action.payload.check
+            if(action.payload.name === 'Design for ultimate limit state'){
+                state.analyze[index].check = action.payload.check
+                if(action.payload.check){
+                    const index2 = state.analyze.findIndex((item) => item.name === 'Design for serviceability');
+                    state.analyze[index2].check = false;
+                }
+            }else if(action.payload.name === 'Design for serviceability'){
+                state.analyze[index].check = action.payload.check
+                if(action.payload.check){
+                    const index2 = state.analyze.findIndex((item) => item.name === 'Design for ultimate limit state');
+                    state.analyze[index2].check = false;
+                }
+            }else{
+                state.analyze[index].check = action.payload.check
+            }
         }
     }
 })
