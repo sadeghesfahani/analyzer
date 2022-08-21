9a4226e79d0df5e2acc18901e9fa136690b063f0
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-21 17:54:10 +0430

    feat(seismicPerformanceFactors):condition added

--- pages/seismicPerformanceFactors.jsx
+++ pages/seismicPerformanceFactors.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 
-function SeismicPerformanceFactors() {
-  return (
-    <div className='flex justify-between h-full'>
+const Res = () => (
+  <div className='flex justify-between h-full'>
         <div className='text-4xl p-12'>
-            <h1>R = 5 - 4</h1>
-            <h1>Cd = 6 - 7</h1>
-            <h1>Omega = 2 - 3</h1>
+            <h1 className='italic'>R = 5 - 4</h1>
+            <h1 className='italic flex'>C<h6 className='text-[24px] mt-3'>d</h6> = 6 - 7</h1>
+            <h1 className='italic'>Omega = 2 - 3</h1>
         </div>
         <img src="result3.png" className='h-full' alt="" />
     </div>
+)
+
+function SeismicPerformanceFactors() {
+  const [frame, setFrame] = useState("")
+  useEffect(() => {
+    if (window) {
+      electron.ipcRenderer
+        .invoke("get-file-data")
+        .then((res) => {
+          setFrame(res.seismicResistingFramesType);
+        })
+        .catch((e) => console.log(e));
+    }
+  }, []);
+  return (
+    <>
+    {frame === "Braced frame" ? <Res/> : 
+      <div className='h-full flex justify-center items-center'>
+        <p>
+      You have selected &quot;Moment frame&quot;, please check this window in the future
+      </p>
+      </div>
+    }
+    </>
   )
 }
 
