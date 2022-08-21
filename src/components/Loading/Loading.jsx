9b560aa35c7c4c0aecf1280af82e27e128527bca
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-08-21 16:04:43 +0430

    padding changes to loading

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -6,7 +6,7 @@ export default function Loading(props) {
     }
 
         return (
-            <div className="flex flex-col gap-4 justify-center items-center h-full w-full px-10">
+            <div className="flex flex-col gap-4 justify-center items-center h-full w-full px-[10%]">
                 <div className="w-full bg-gray-200 h-3 rounded overflow-hidden ">
                     <div className="bg-red-700 h-3" style={{width: `${props.progress}%`}}></div>
                 </div>
