90b11d85ca359bdd6632d719e84bf46b5180b7c1
mohammadyousefi02 <mohammadyousefi02@gmail.com>
2022-08-21 19:15:35 +0430

    feat(aboutus):created

---
+++ pages/aboutus.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AboutUs() {
+  return (
+    <div className='h-full flex justify-center flex-col items-center'>
+        <h1>Sina Sadegh Esfahani</h1>
+        <h1>Sina Taghavi</h1>
+        <h1>Mohammad Yousefi</h1>
+        <h1>Sama-It</h1>
+    </div>
+  )
+}
+
+export default AboutUs
