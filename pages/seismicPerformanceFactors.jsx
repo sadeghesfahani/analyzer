c61f4df6c07ef06bc6d65457e69ce1e33e6052fa
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-08-21 16:24:52 +0430

    seismicPerformanceFactors.jsx clean-up

--- pages/seismicPerformanceFactors.jsx
+++ pages/seismicPerformanceFactors.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 
 function SeismicPerformanceFactors() {
-  return (
-    <div className='flex justify-between h-full'>
-        <div className='text-4xl p-12'>
-            <h1>R = 5 - 4</h1>
-            <h1>Cd = 6 - 7</h1>
-            <h1>Omega = 2 - 3</h1>
+    return (
+        <div className='flex items-center justify-between h-full'>
+            <div className='flex text-3xl font-bold flex-col items-center gap-1 w-48 relative bottom-28 left-28 '>
+                <div className="flex  flex-row justify-between w-full ">
+                    <h1 className="w-1/4">R</h1>
+                    <h1 className="w-1/4">=</h1>
+                    <h1 className="w-2/4">5 - 4</h1>
+                </div>
+                <div className="flex flex-row justify-between w-full ">
+                    <h1 className="w-1/4">C<sub>d</sub></h1>
+                    <h1 className="w-1/4">=</h1>
+                    <h1 className="w-2/4">6 - 7</h1>
+                </div>
+                <div className="flex flex-row justify-between w-full ">
+                    <h1 className="w-1/4">Ω</h1>
+                    <h1 className="w-1/4">=</h1>
+                    <h1 className="w-2/4">2 - 3</h1>
+                </div>
+            </div>
+            <img src="result3.png" className='h-full' alt=""/>
         </div>
-        <img src="result3.png" className='h-full' alt="" />
-    </div>
-  )
+    )
 }
 
 export default SeismicPerformanceFactors
