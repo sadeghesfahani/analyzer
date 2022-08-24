4429c5d9a2880bcd2d41d3efc3ffca50cc637042
Alireza Taghavi <taghavizadeh.alireza@gmail.com>
2022-08-24 13:30:36 +0430

    border for results

--- src/components/Result/index.jsx
+++ src/components/Result/index.jsx
@@ -1,180 +1,182 @@
 import React from "react";
+import SectionTitle from "../SectionTitle";
 
 const Line = () => "-------------------------------------------------------";
 
 function Result({
-  bottom,
-  left,
-  coI33,
-  coMe,
-  fI33,
-  fMe,
-  ceI33,
-  ceMe,
-  gpi,
-  gpe,
-  u1,
-  u2,
-  u3,
-  r1,
-  r2,
-  r3,
-  intraU1,
-  intraU2,
-  intraU3,
-  intraR1,
-  intraR2,
-  intraR3,
-  analyze1,
-  analyze2,
-  analyze3,
-  heightOfStorey,
-  lengthOfSpan,
-  seismicResistingFramesType,
-  report,
-  rho,
-}) {
-  let ELFimg = "";
-  let boundaryImg = "";
-  if (seismicResistingFramesType === "Braced frame") {
-    if (report === "rigid") {
-      ELFimg = "Braced Frame/Rigid/ELF.png";
-      boundaryImg = "Braced Frame/Rigid/Rigid Boundary.png";
-    } else if (rho < 0.2) {
-      ELFimg = "Braced Frame/Rho = 0.01/ELF.png";
-      boundaryImg = "Braced Frame/Rho = 0.01/Rigid Boundary.png";
+                    bottom,
+                    left,
+                    coI33,
+                    coMe,
+                    fI33,
+                    fMe,
+                    ceI33,
+                    ceMe,
+                    gpi,
+                    gpe,
+                    u1,
+                    u2,
+                    u3,
+                    r1,
+                    r2,
+                    r3,
+                    intraU1,
+                    intraU2,
+                    intraU3,
+                    intraR1,
+                    intraR2,
+                    intraR3,
+                    analyze1,
+                    analyze2,
+                    analyze3,
+                    heightOfStorey,
+                    lengthOfSpan,
+                    seismicResistingFramesType,
+                    report,
+                    rho,
+                }) {
+    let ELFimg = "";
+    let boundaryImg = "";
+    if (seismicResistingFramesType === "Braced frame") {
+        if (report === "rigid") {
+            ELFimg = "Braced Frame/Rigid/ELF.png";
+            boundaryImg = "Braced Frame/Rigid/Rigid Boundary.png";
+        } else if (rho < 0.2) {
+            ELFimg = "Braced Frame/Rho = 0.01/ELF.png";
+            boundaryImg = "Braced Frame/Rho = 0.01/Rigid Boundary.png";
+        } else {
+            ELFimg = "Braced Frame/Rho = 0.2/ELF.png";
+            boundaryImg = "Braced Frame/Rho = 0.2/Rigid Boundary.png";
+        }
     } else {
-      ELFimg = "Braced Frame/Rho = 0.2/ELF.png";
-      boundaryImg = "Braced Frame/Rho = 0.2/Rigid Boundary.png";
+        if (analyze1) {
+            if (report === "rigid") {
+                ELFimg = "Moment Frame/Ultimate/Rigid/ELF.png";
+                boundaryImg = "Moment Frame/Ultimate/Rigid/Rigid Boundary.png";
+            } else if (rho < 0.05) {
+                ELFimg = "Moment Frame/Ultimate/Rho = 0.01/ELF.png";
+                boundaryImg = "Moment Frame/Ultimate/Rho = 0.01/Rigid Boundary.png";
+            } else if (rho < 0.1) {
+                ELFimg = "Moment Frame/Ultimate/Rho = 0.05/ELF.png";
+                boundaryImg = "Moment Frame/Ultimate/Rho = 0.05/Rigid Boundary.png";
+            } else {
+                ELFimg = "Moment Frame/Ultimate/Rho = 0.10/ELF.png";
+                boundaryImg = "Moment Frame/Ultimate/Rho = 0.10/Rigid Boundary.png";
+            }
+        } else {
+            if (report === "rigid") {
+                ELFimg = "Moment Frame/Ultimate/Rigid/ELF.png";
+                boundaryImg = "Moment Frame/Drift/Rigid/Boundary.png";
+            } else if (rho < 0.05) {
+                ELFimg = "Moment Frame/Ultimate/Rho = 0.01/ELF.png";
+                boundaryImg = "Moment Frame/Drift/Rho = 0.01/Rigid Boundary.png";
+            } else if (rho < 0.1) {
+                ELFimg = "Moment Frame/Ultimate/Rho = 0.05/ELF.png";
+                boundaryImg = "Moment Frame/Drift/Rho = 0.05/Rigid Boundary.png";
+            } else {
+                ELFimg = "Moment Frame/Ultimate/Rho = 0.10/ELF.png";
+                boundaryImg = "Moment Frame/Drift/Rho = 0.1/Rigid Boundary.png";
+            }
+        }
     }
-  } else {
-    if (analyze1) {
-      if (report === "rigid") {
-        ELFimg = "Moment Frame/Ultimate/Rigid/ELF.png";
-        boundaryImg = "Moment Frame/Ultimate/Rigid/Rigid Boundary.png";
-      } else if (rho < 0.05) {
-        ELFimg = "Moment Frame/Ultimate/Rho = 0.01/ELF.png";
-        boundaryImg = "Moment Frame/Ultimate/Rho = 0.01/Rigid Boundary.png";
-      } else if (rho < 0.1) {
-        ELFimg = "Moment Frame/Ultimate/Rho = 0.05/ELF.png";
-        boundaryImg = "Moment Frame/Ultimate/Rho = 0.05/Rigid Boundary.png";
-      } else {
-        ELFimg = "Moment Frame/Ultimate/Rho = 0.10/ELF.png";
-        boundaryImg = "Moment Frame/Ultimate/Rho = 0.10/Rigid Boundary.png";
-      }
-    } else {
-      if (report === "rigid") {
-        ELFimg = "Moment Frame/Ultimate/Rigid/ELF.png";
-        boundaryImg = "Moment Frame/Drift/Rigid/Boundary.png";
-      } else if (rho < 0.05) {
-        ELFimg = "Moment Frame/Ultimate/Rho = 0.01/ELF.png";
-        boundaryImg = "Moment Frame/Drift/Rho = 0.01/Rigid Boundary.png";
-      } else if (rho < 0.1) {
-        ELFimg = "Moment Frame/Ultimate/Rho = 0.05/ELF.png";
-        boundaryImg = "Moment Frame/Drift/Rho = 0.05/Rigid Boundary.png";
-      } else {
-        ELFimg = "Moment Frame/Ultimate/Rho = 0.10/ELF.png";
-        boundaryImg = "Moment Frame/Drift/Rho = 0.1/Rigid Boundary.png";
-      }
-    }
-  }
-  return (
-    <div className="flex h-full">
-      <div className="flex-1 border h-full flex flex-col gap-8 overflow-y-auto p-1">
-        {analyze1 && (
-          <div className="h-1/2 flex flex-col gap-1 items-center">
-            <span>Effective Length Factor</span>
-            <img src={ELFimg} alt="" className=" h-full object-contain border p-1" />
-          </div>
-        )}
-        <div className={`relative flex flex-col gap-1 items-center ${analyze1 ? "h-1/2" : "h-full"}`}>
-        <span className="text-center w-full">{report === "rigid" ? "Rigid" : "Semi-rigid Boundary"}</span>
-          <img
-            src={boundaryImg}
-            alt=""
-            className=" h-full object-contain border"
-          />
-          <div
-            style={{ bottom: bottom, left: left }}
-            className={`text-red-700 absolute text-4xl font-bold`}
-          >
-            *
-        </div>
-        </div>
-      </div>
-      <div className="flex-1 border h-full text-xs overflow-y-auto px-1">
-        <Line />
-        <p>Project_Name : untitled</p>
-        <Line />
-        <p>
-          Gpi =(Material_Ecolumn*Section_I33column/Lcolumn) / (Material_Efloor
-          beam*Section_I33 floor beam/Lfloorbeam)
-        </p>
-        <p>
-          Gpi=({coMe}*{coI33}/{heightOfStorey}) / ({fMe}*{fI33}/{lengthOfSpan})
-          = {gpi}
-        </p>
-        <Line />
-        <p>
-          Gpe=(Material_ Ecolumn*Section_I33column/Lcolumn) / (Material_
-          Eceiling beam*Section_I33 ceiling beam/Lceilingbeam)
-        </p>
-        <p>
-          Gpe=({coMe}*{coI33}/{heightOfStorey}) / ({ceMe}*{ceI33}/{lengthOfSpan}
-          ) = {gpe}
-        </p>
-        <Line />
-        <p>Lcolumn {heightOfStorey}: </p>
-        <p>Lfloor_beam : {lengthOfSpan}</p>
-        <p>Lceiling_beam : {lengthOfSpan}</p>
-        <Line />
-        <p>Section - Columns - i33 : {coI33}</p>
-        <p>Section - Floor - i33 : {fI33}</p>
-        <p>Section - Ceiling - i33 : {ceI33}</p>
-        <p>Material - Columns - Me : {coMe}</p>
-        <p>Material - Floor - Me : {fMe}</p>
-        <p>Material - Ceiling - Me : {ceMe}</p>
-        <Line />
-        <p>inter - U1 : {u1}</p>
-        <p>inter - U2 : {u2}</p>
-        <p>inter - U3 : {u3}</p>
-        <p>inter - R1 : {r1}</p>
-        <p>inter - R2 : {r2}</p>
-        <p>inter - R3 : {r3}</p>
-        <Line />
-        <p>intra - U1 : {intraU1}</p>
-        <p>intra - U2 : {intraU2}</p>
-        <p>intra - U3 : {intraU3}</p>
-        <p>intra - R1 : {intraR1}</p>
-        <p>intra - R2 : {intraR2}</p>
-        <p>intra - R3 : {intraR3}</p>
-        <Line />
-        <p>
-          Analyze: Design_For_Ultimate_Limit_State :{" "}
-          {analyze1 ? "True" : "False"}
-        </p>
-        <p>
-          Analyze: Design_For_Service_Ability : {analyze2 ? "True" : "False"}
-        </p>
-        <p>
-          Analyze: Seismic_Performance_Factors : {analyze3 ? "True" : "False"}
-        </p>
-        <Line />
-        <p>Structure__Height_of_storey : {heightOfStorey}</p>
-        <p>Structure__Length_of_span : {lengthOfSpan}</p>
-        <Line />
-        <p>Seismic_resisting_frames_type : {seismicResistingFramesType}</p>
-        <Line />
-      </div>
-      {/* <div className="flex-1 h-full relative border">
+    return (
+        <div className="flex h-full">
+            <div className="flex-1 border h-full flex flex-col gap-6 overflow-y-auto py-4 px-24">
+                {analyze1 && (
+                    <div className="h-1/2 relative  flex flex-col border gap-1 items-center py-1">
+                        <SectionTitle className="text-sm" title="Effective Length Factor"/>
+                        <img src={ELFimg} alt="" className=" h-full object-contain"/>
+                    </div>
+                )}
+                <div className={`relative flex flex-col border gap-1 items-center py-1 ${analyze1 ? "h-1/2" : "h-full"}`}>
+                    <SectionTitle className="text-sm" title={report === "rigid" ? "Rigid" : "Semi-rigid Boundary"}/>
+                    {/*<span className="text-center w-full">{report === "rigid" ? "Rigid" : "Semi-rigid Boundary"}</span>*/}
+                    <img
+                        src={boundaryImg}
+                        alt=""
+                        className=" h-full object-contain "
+                    />
+                    <div
+                        style={{bottom: bottom, left: left}}
+                        className={`text-red-700 absolute text-4xl font-bold`}
+                    >
+                        *
+                    </div>
+                </div>
+            </div>
+            <div className="flex-1 border h-full text-xs overflow-y-auto px-1">
+                <Line/>
+                <p>Project_Name : untitled</p>
+                <Line/>
+                <p>
+                    Gpi =(Material_Ecolumn*Section_I33column/Lcolumn) / (Material_Efloor
+                    beam*Section_I33 floor beam/Lfloorbeam)
+                </p>
+                <p>
+                    Gpi=({coMe}*{coI33}/{heightOfStorey}) / ({fMe}*{fI33}/{lengthOfSpan})
+                    = {gpi}
+                </p>
+                <Line/>
+                <p>
+                    Gpe=(Material_ Ecolumn*Section_I33column/Lcolumn) / (Material_
+                    Eceiling beam*Section_I33 ceiling beam/Lceilingbeam)
+                </p>
+                <p>
+                    Gpe=({coMe}*{coI33}/{heightOfStorey}) / ({ceMe}*{ceI33}/{lengthOfSpan}
+                    ) = {gpe}
+                </p>
+                <Line/>
+                <p>Lcolumn {heightOfStorey}: </p>
+                <p>Lfloor_beam : {lengthOfSpan}</p>
+                <p>Lceiling_beam : {lengthOfSpan}</p>
+                <Line/>
+                <p>Section - Columns - i33 : {coI33}</p>
+                <p>Section - Floor - i33 : {fI33}</p>
+                <p>Section - Ceiling - i33 : {ceI33}</p>
+                <p>Material - Columns - Me : {coMe}</p>
+                <p>Material - Floor - Me : {fMe}</p>
+                <p>Material - Ceiling - Me : {ceMe}</p>
+                <Line/>
+                <p>inter - U1 : {u1}</p>
+                <p>inter - U2 : {u2}</p>
+                <p>inter - U3 : {u3}</p>
+                <p>inter - R1 : {r1}</p>
+                <p>inter - R2 : {r2}</p>
+                <p>inter - R3 : {r3}</p>
+                <Line/>
+                <p>intra - U1 : {intraU1}</p>
+                <p>intra - U2 : {intraU2}</p>
+                <p>intra - U3 : {intraU3}</p>
+                <p>intra - R1 : {intraR1}</p>
+                <p>intra - R2 : {intraR2}</p>
+                <p>intra - R3 : {intraR3}</p>
+                <Line/>
+                <p>
+                    Analyze: Design_For_Ultimate_Limit_State :{" "}
+                    {analyze1 ? "True" : "False"}
+                </p>
+                <p>
+                    Analyze: Design_For_Service_Ability : {analyze2 ? "True" : "False"}
+                </p>
+                <p>
+                    Analyze: Seismic_Performance_Factors : {analyze3 ? "True" : "False"}
+                </p>
+                <Line/>
+                <p>Structure__Height_of_storey : {heightOfStorey}</p>
+                <p>Structure__Length_of_span : {lengthOfSpan}</p>
+                <Line/>
+                <p>Seismic_resisting_frames_type : {seismicResistingFramesType}</p>
+                <Line/>
+            </div>
+            {/* <div className="flex-1 h-full relative border">
         <img src={boundaryImg} alt="" className="" />
         <div
           style={{ bottom: bottom, left: left }}
           className={`text-red-600 absolute text-4xl font-bold`}
         >*</div>
       </div> */}
-    </div>
-  );
+        </div>
+    );
 }
 
 export default Result;
